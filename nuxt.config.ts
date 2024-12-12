b7b85619dd0caa1a2f3e8893a5bc02df506ce2ea
Arash Ari Sheyda <arashi.sheyda@gmail.com>
2024-12-11 19:20:59 -0700

    fix: components prefix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,7 @@
+import { createResolver } from '@nuxt/kit'
+
+const { resolve } = createResolver(import.meta.url)
+
 export default defineNuxtConfig({
 
   modules: [
@@ -11,6 +15,15 @@ export default defineNuxtConfig({
 
   devtools: {
     enabled: true,
+  },
+
+  components: {
+    dirs: [
+      {
+        prefix: 'V',
+        path: resolve('./components'),
+      },
+    ],
   },
 
   colorMode: {
