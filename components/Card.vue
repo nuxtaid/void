dbad4b8377c3a162f7165d31a6e08a2b9a0c5618
Arash Ari Sheyda <arashi.sheyda@gmail.com>
2024-12-11 21:00:46 -0700

    feat(components): add card component

---
+++ components/Card.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { NuxtLink } from '#components'
+
+const props = withDefaults(defineProps<{
+  title: string
+  description: string
+  color?: string
+  frame?: string
+  path?: string
+  target?: string
+}>(), {
+  color: '',
+})
+
+const linkTarget = computed(() => props.path ? props.target || (props.path.startsWith('http') ? '_blank' : undefined) : undefined)
+</script>
+
+<template>
+  <component
+    :is="path ? NuxtLink : 'div'"
+    :to="path"
+    :target="linkTarget"
+    class="flex flex-col gap-4 justify-between border border-[#2A2A29] p-8 rounded bg-[#1A1A1A] h-96 transition-border duration-300 hover:border-[--void-card-border]"
+    :class="{ 'no-underline': path }"
+    :style="{
+      '--void-card-border': color,
+    }"
+  >
+    <div>
+      <!-- TODO: replace this -->
+      <slot name="image">
+        <iframe
+          v-if="frame"
+          :src="frame"
+          class="lg:w-[21rem] lg:h-[11.8rem] h-[15rem] w-full rounded-lg pointer-events-none"
+        />
+      </slot>
+    </div>
+    <div>
+      <slot name="title">
+        <h2>
+          {{ title }}
+        </h2>
+      </slot>
+      <slot name="description">
+        <p>
+          {{ description }}
+        </p>
+      </slot>
+    </div>
+  </component>
+</template>
