e8041463927fd4a3c1fa76ee4778f12ef0ff1832
Arash Ari Sheyda <arashi.sheyda@gmail.com>
2024-12-11 21:01:00 -0700

    feat(components): add grid component

---
+++ components/Grid.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+  items: any[]
+  reverse?: boolean
+}>()
+
+const filtered = computed(() => props.reverse ? props.items.slice().reverse() : props.items)
+</script>
+
+<template>
+  <section class="grid md:grid-cols-2 xl:grid-cols-3 gap-4 my-4">
+    <VCard
+      v-for="item in filtered"
+      :key="item.id"
+      v-bind="item"
+    />
+    <slot />
+  </section>
+</template>
