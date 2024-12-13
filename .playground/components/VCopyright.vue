a81619c1a4f98744d590838f698b62ca5c82d632
Arash Ari Sheyda <arashi.sheyda@gmail.com>
2024-12-12 21:15:03 -0700

    update copyright

---
+++ .playground/components/VCopyright.vue
@@ -0,0 +1,13 @@
+<template>
+  void (WIP) by
+  <NuxtLink
+    to="https://github.com/nuxtaid"
+    target="_blank"
+  >
+    NuxtAid
+  </NuxtLink>
+  | Hosted on
+  <span title="1 user at a time, please :)">
+    Raspberry Pi 5
+  </span>
+</template>
