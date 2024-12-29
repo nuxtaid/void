a47afceaeb1f1987cae60939c906613f70029171
Arash Ari Sheyda <arashi.sheyda@gmail.com>
2024-12-28 21:20:35 -0700

    feat: add socials

---
+++ components/Socials.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+const config = useVoid()
+</script>
+
+<template>
+  <div class="flex items-center gap-4">
+    <NuxtLink
+      v-for="item in config.socials"
+      :key="item.url"
+      class="social-item"
+      :to="item.url"
+      :rel="item?.target === '_blank' ? 'noopener noreferrer' : ''"
+      :target="item.target"
+      :title="item.title"
+    >
+      <Icon
+        :name="item.icon"
+        :class="item.title"
+        transition-all
+        :style="{ '--void-social-hover': item.color }"
+      />
+    </NuxtLink>
+  </div>
+</template>
+
+<style>
+.social-item:hover svg {
+  color: var(--void-social-hover);
+  filter: drop-shadow(0 0 0.75rem var(--void-social-hover));
+}
+</style>
