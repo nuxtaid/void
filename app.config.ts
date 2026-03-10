040b7975e6ac3f7bea79a5bc5ac7351080afb10a
Arash Ari Sheyda <hi@arashsheyda.me>
2026-03-09 21:03:12 -0600

    fix: update social links

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,37 @@
 export default defineAppConfig({
   void: {
+    name: 'Arash Sheyda',
+    avatar: '/assets/01.jpg',
+    title: 'a passionate <br/> Software Engineer',
+    description: 'with a <span class="text-red-600">love</span> for building creative and meaningful solutions. Whether I\'m coding or exploring the <span class="text-cyan-500">skies</span>, I thrive on discovery and problem-solving. Outside of tech, you\'ll find me enjoying nature, good food, and the occasional DIY adventure.',
+    socials: [
+      {
+        title: 'Github',
+        icon: 'uil:github',
+        url: 'https://github.com/nuxtaid/void',
+        target: '_blank',
+      },
+      {
+        title: 'Twitter',
+        icon: 'uil:twitter',
+        url: 'https://twitter.com/arash_sheyda',
+        color: '#1da1f2',
+        target: '_blank',
+      },
+      {
+        title: 'Bluesky',
+        icon: 'i-simple-icons-bluesky',
+        url: 'https://twitter.com/arash_sheyda',
+        color: '#0385FF',
+        target: '_blank',
+      },
+      {
+        title: 'LinkedIn',
+        icon: 'uil:linkedin',
+        url: 'https://www.linkedin.com/in/arash-sheyda/',
+        color: '#0a66c2',
+        target: '_blank',
+      },
+    ],
   },
 })
