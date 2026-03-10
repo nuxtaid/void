7fabde539b6969d1acabb86966798878c22b52aa
Arash Ari Sheyda <hi@arashsheyda.me>
2026-03-09 21:46:24 -0600

    feat(components): add section component

---
+++ components/Section.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string
+  icon?: string
+  padding?: string
+}>()
+</script>
+
+<template>
+  <div
+    :class="{ 'md:p-4': padding !== 'false' }"
+  >
+    <h2
+      v-if="title"
+      class="flex items-center gap-2 text-2xl mb-4"
+    >
+      <Icon
+        v-if="icon"
+        :name="icon"
+      />
+      {{ title }}
+    </h2>
+    <slot />
+  </div>
+</template>
